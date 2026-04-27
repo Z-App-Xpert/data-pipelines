fe338f316bdf851fde397d0bd40cb122b12e4ebd
Zielinski <Edward.Zielinski@va.gov>
2026-04-27 15:21:02 -0400

    Added make dir line

--- scripts/transformXML.py
+++ scripts/transformXML.py
@@ -4,6 +4,7 @@ import os
 
 today = datetime.today().strftime("%Y-%m-%d")   
 output_dir = os.path.join(os.path.dirname(__file__), "..","output", today)
+os.makedirs(output_dir, exist_ok=True)
 file_path = os.path.join(output_dir, "latestHumanlist.xml")                        
 
 url = "https://assets.hpra.ie/products/xml/latestHumanlist.xml"
